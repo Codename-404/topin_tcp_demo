bcd80ce1517c1b8018bfb16864de2970ea751287
Codename-404 <nayeem.nayeem712@gmail.com>
2024-09-21 06:50:02 +0600

    simple setup

--- server.js
+++ server.js
@@ -1,70 +1,37 @@
-require("dotenv").config();
-const { default: axios } = require("axios");
 const net = require("net");
-const express = require("express");
 
 // Define the port where the server will listen for connections
 const PORT = 5005;
 
 // Create a server instance
-// const server = net.createServer((socket) => {
-//   console.log("New device connected");
+const server = net.createServer((socket) => {
+  console.log("New device connected");
 
-//   // Event handler for receiving data from the client (GPS tracker)
-//   socket.on("data", (data) => {
-//     // Convert the binary data (Buffer) to a string
-//     const message = data.toString();
-//     console.log("Received data:", message);
+  // Event handler for receiving data from the client (GPS tracker)
+  socket.on("data", (data) => {
+    // Convert the binary data (Buffer) to a string
+    const message = data.toString();
+    console.log("Received data:", message);
 
-//     // Decoding the Topin protocol message here depending on the message structure
-//     // let's discuss further for this step
+    // Decoding the Topin protocol message here depending on the message structure
+    // let's discuss further for this step and how would you like to add more functionalities
 
-//     // For now, send a response to the GPS device
-//     socket.write("Found You!");
-//   });
-
-//   // Handle connection errors
-//   socket.on("error", (err) => {
-//     console.error("Connection error:", err);
-//   });
-
-//   // Handle connection closure
-//   socket.on("close", () => {
-//     console.log("Client Connection closed");
-//   });
-// });
-
-const fetchMessages = async () => {
-  const url = `https://flespi.io/gw/channels/${process.env.CHANNEL_ID}/messages`;
-  const token = process.env.TOKEN;
-
-  console.log();
-  const res = await axios.get(url, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `FlespiToken ${token}`,
-    },
+    // For now, send a response to the GPS device
+    socket.write("Found You!");
   });
 
-  console.log(res.data);
-  return res.data;
-};
+  // Handle connection errors
+  socket.on("error", (err) => {
+    console.error("Connection error:", err);
+  });
 
-fetchMessages();
-// Start listening for connections
-// server.listen(PORT, () => {
-//   console.log(`GPS server listening on port ${PORT}`);
-// });
-
-const app = express();
-
-// Define a simple GET route
-app.get("/", async (req, res) => {
-  const data = await fetchMessages();
-
-  res.send(data);
+  // Handle connection closure
+  socket.on("close", () => {
+    console.log("Client Connection closed");
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start listening for connections
+server.listen(PORT, () => {
+  console.log(`GPS server listening on port ${PORT}`);
 });
